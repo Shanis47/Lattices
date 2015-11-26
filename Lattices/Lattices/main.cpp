ce7fb22e064d45b9d15a1aa032452be58acec8ab
Shanis47 <Shanis394042@mail.ru>
2015-11-26 10:04:35 +0300

    Fix errors after merge

--- Lattices/Lattices/main.cpp
+++ Lattices/Lattices/main.cpp
@@ -1,8 +1,12 @@
 #include "Lattice.h"
 #include <iostream>
+#include <fstream>
 using namespace std;
 
 int Menu();
+Lattice* InputFromKeyboard();
+Lattice* InputFromFile();
+void PrintLattice(Lattice* );
 
 int main()
 {
@@ -47,6 +51,86 @@ int Menu()
 	cout << "0. Exit" << endl;
 	int result;
 	cin >> result;
+
+	return result;
+}
+
+Lattice* InputBasis(uint size, istream &input);
+
+Lattice* InputFromKeyboard()
+{
+	int size;
+	double** basis;
+	cout << "size = ";
+	cin >> size;
+
+	return InputBasis(size, cin);
+}
+
+Lattice* InputFromFile()
+{
+	char fileName[256];
+	cout << "file name: ";
+	cin >> fileName;
+	
+	ifstream inputFile;
+	inputFile.open(fileName);
+
+	int size;
+	inputFile >> size;
+	return InputBasis(size, inputFile);
+}
+
+void PrintLattice(Lattice* lattice)
+{
+	int size = lattice->GetSize();
+
+	if (!size)
+	{
+		cout << "Basis not setted!" << endl;
+		return;
+	}
+
+	double** basis = lattice->GetBasis();
+
+	for (int i = 0; i < size; i++)
+	{
+		for (int j = 0; j < size; j++)
+			cout << basis[i][j] << "\t";
+		cout << endl;
+	}
+
+	for (int i =0; i< size; i++)
+		delete[] basis[i];
+	delete[] basis;
+}
+
+Lattice* InputBasis(uint size, istream &input)
+{
+	double** basis;
+	basis = new double*[size];
+	
+	for (int i = 0; i < size; i++)
+	{
+		basis[i] = new double[size];
+		for (int j = 0; j < size; j++)
+			input >> basis[i][j];
+	}
+
+	Lattice* result;
+	try
+	{
+		result = new Lattice(size, basis);
+	}
+	catch (exception &e)
+	{
+		cout << e.what() << endl;
+		result = new Lattice();
+	}
+
+	for (int i =0; i< size; i++)
+		delete[] basis[i];
+	delete[] basis;
 
 	return result;
 }
